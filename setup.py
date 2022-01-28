661b047e9bbeb65e50d149e7dc19c217052e8b89
gbirolo <41187445+gbirolo@users.noreply.github.com>
2022-01-28 16:52:46 +0100

    removed leftover check

--- setup.py
+++ setup.py
@@ -36,12 +36,7 @@ PACKAGES = [
 
 
 if __name__ == "__main__":
-
     from setuptools import setup
-
-    import sys
-    if sys.version_info[:2] < (3, 7):
-        raise RuntimeError("seaborn requires python >= 3.7.")
 
     setup(
         name=DISTNAME,
